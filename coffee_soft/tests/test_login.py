d4a6f2e74bc5fc9a58f59e55b9b6594be8774536
mosheavidor1 <mosheavidorster@gmail.com>
2024-04-12 13:38:42 +0300

    Setting browsers

--- coffee_soft/tests/test_login.py
+++ coffee_soft/tests/test_login.py
@@ -1,67 +1,14 @@
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-#
-#
-# @pytest.fixture
-# def driver():
-#     chrome_options = Options()
-#     chrome_options.add_experimental_option("detach", True)
-#     chrome_options.add_argument("--start-maximized")
-#     driver = webdriver.Chrome(options=chrome_options)
-#     yield driver
-#
-#
-# def test_google_title(driver):
-#     driver.get("https://google.com")
-#     assert "Google" in driver.title
-
-
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.edge.options import Options as EdgeOptions
+from coffee_soft.utils.drivers.driver_factory import DriverFactory
 
 
-class DriverFactory:
-    def __init__(self, browser='chrome'):
-        self.browser = browser.lower()
-        self.driver = None
+class TestLogin:
+    @pytest.fixture
+    def driver(self):
+        driver_factory = DriverFactory('chrome')
+        driver = driver_factory.create_driver()
+        yield driver
+        driver_factory.quit_driver()
 
-    def create_driver(self, maximize_window=True, detach=True):
-        if self.browser == 'chrome':
-            chrome_options = ChromeOptions()
-            if maximize_window:
-                chrome_options.add_argument('--start-maximized')
-            if detach:
-                chrome_options.add_experimental_option('detach', True)
-            self.driver = webdriver.Chrome(options=chrome_options)
-        elif self.browser == 'firefox':
-            firefox_options = FirefoxOptions()
-            if maximize_window:
-                firefox_options.add_argument('--start-maximized')
-            if detach:
-                firefox_options.add_argument('--detach')
-            self.driver = webdriver.Firefox(options=firefox_options)
-        elif self.browser == 'edge':
-            edge_options = EdgeOptions()
-            if maximize_window:
-                edge_options.add_argument('--start-maximized')
-            if detach:
-                edge_options.add_argument('--browser.detachUrl')
-            self.driver = webdriver.Edge(options=edge_options)
-        else:
-            raise ValueError(f"Invalid browser: {self.browser}")
-        return self.driver
-
-
-@pytest.fixture
-def driver():
-    driver_factory = DriverFactory('chrome')
-    driver = driver_factory.create_driver()
-    yield driver
-
-
-def test_google_title(driver):
-    driver.get("https://google.com")
+    def test_google_title(self, driver):
+        driver.get("https://google.com")
--- coffee_soft/utils/drivers/driver_factory.py
+++ coffee_soft/utils/drivers/driver_factory.py
@@ -1,31 +1,36 @@
-from webdriver_manager.firefox import GeckoDriverManager
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 
 class DriverFactory:
-    def __init__(self):
+    def __init__(self, browser='chrome'):
+        self.browser = browser.lower()
         self.driver = None
 
-    def set_driver(self, browser):
-        if browser == "chrome":
-            chrome_options = Options()
-            self.driver = webdriver.Chrome(chrome_options=chrome_options)
-
-        elif browser == "firefox":
-            self.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-        elif browser == "edge":
-            self.driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+    def create_driver(self, maximize_window=True, detach=True):
+        if self.browser == 'chrome':
+            chrome_options = ChromeOptions()
+            if maximize_window:
+                chrome_options.add_argument('--start-maximized')
+            if detach:
+                chrome_options.add_experimental_option('detach', True)
+            self.driver = webdriver.Chrome(options=chrome_options)
+        elif self.browser == 'firefox':
+            firefox_options = FirefoxOptions()
+            if maximize_window:
+                firefox_options.add_argument('--start-maximized')
+            if detach:
+                firefox_options.add_argument('--detach')
+            self.driver = webdriver.Firefox(options=firefox_options)
+        elif self.browser == 'edge':
+            edge_options = EdgeOptions()
+            if maximize_window:
+                edge_options.add_argument('--start-maximized')
+            if detach:
+                edge_options.add_argument('--browser.detachUrl')
+            self.driver = webdriver.Edge(options=edge_options)
         else:
-            print("Browser does not exist")
-
-    def get_driver(self):
-        return self.driver
-
-    def set_url(self, url):
-        if self.driver:
-            self.driver.get(url)
-
-    def create_driver(self):
+            raise ValueError(f"Invalid browser: {self.browser}")
         return self.driver
